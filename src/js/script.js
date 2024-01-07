a31c74aa5cc77edc3a54f19d86f76fbc91ed204a
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2024-01-07 20:08:02 +0400

    🚧 Worked on the navigation menu.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,33 @@
+// Navigation Menu
+const header = document.querySelector(".header");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const closeMenu = document.querySelector(".close-menu");
+const navigationMenu = document.querySelector(".navigation-menu");
+const navigationMenuLink = document.querySelectorAll(".navigation-menu__link");
+
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.add("hamburger-menu--unactive");
+  header.classList.add("header--active");
+  closeMenu.classList.add("close-menu--active");
+  navigationMenu.classList.add("navigation-menu--active");
+});
+
+closeMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.remove("hamburger-menu--unactive");
+  header.classList.remove("header--active");
+  closeMenu.classList.remove("close-menu--active");
+  navigationMenu.classList.remove("navigation-menu--active");
+});
+
+navigationMenuLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("hamburger-menu--unactive");
+    header.classList.remove("header--active");
+    closeMenu.classList.remove("close-menu--active");
+    navigationMenu.classList.remove("navigation-menu--active");
+  });
+});
+
 // Tabs
 const tabs = document.querySelectorAll(".tab");
 const tabContents = document.querySelectorAll(".tab-content");
