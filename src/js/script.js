4fc937beee11bb7c2868760cb9c46eba2e5c81dd
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2024-01-10 00:02:36 +0400

    🎨 Worked on optimisation and fixes.

--- src/js/script.js
+++ src/js/script.js
@@ -6,25 +6,25 @@ const navigationMenu = document.querySelector(".navigation-menu");
 const navigationMenuLink = document.querySelectorAll(".navigation-menu__link");
 
 hamburgerMenu.addEventListener("click", () => {
-  hamburgerMenu.classList.add("hamburger-menu--unactive");
-  header.classList.add("header--active");
-  closeMenu.classList.add("close-menu--active");
-  navigationMenu.classList.add("navigation-menu--active");
+  hamburgerMenu.classList.add("hamburger-menu--hidden");
+  header.classList.add("header--fixed");
+  closeMenu.classList.add("close-menu--visible");
+  navigationMenu.classList.add("navigation-menu--visible");
 });
 
 closeMenu.addEventListener("click", () => {
-  hamburgerMenu.classList.remove("hamburger-menu--unactive");
-  header.classList.remove("header--active");
-  closeMenu.classList.remove("close-menu--active");
-  navigationMenu.classList.remove("navigation-menu--active");
+  closeMenu.classList.remove("close-menu--visible");
+  header.classList.remove("header--fixed");
+  hamburgerMenu.classList.remove("hamburger-menu--hidden");
+  navigationMenu.classList.remove("navigation-menu--visible");
 });
 
 navigationMenuLink.forEach((link) => {
   link.addEventListener("click", () => {
-    hamburgerMenu.classList.remove("hamburger-menu--unactive");
-    header.classList.remove("header--active");
-    closeMenu.classList.remove("close-menu--active");
-    navigationMenu.classList.remove("navigation-menu--active");
+    closeMenu.classList.remove("close-menu--visible");
+    header.classList.remove("header--fixed");
+    hamburgerMenu.classList.remove("hamburger-menu--hidden");
+    navigationMenu.classList.remove("navigation-menu--visible");
   });
 });
 
@@ -35,14 +35,14 @@ const tabContents = document.querySelectorAll(".tab-content");
 tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
     const index = Array.from(tabs).indexOf(tab);
-    const currentTab = document.querySelector(".tab--active");
-    const currentTabContent = document.querySelector(".tab-content--active");
+    const currentTab = document.querySelector(".tab--selected");
+    const currentTabContent = document.querySelector(".tab-content--visible");
 
-    currentTab.classList.remove("tab--active");
-    tab.classList.add("tab--active");
+    currentTab.classList.remove("tab--selected");
+    tab.classList.add("tab--selected");
 
-    currentTabContent.classList.remove("tab-content--active");
-    Array.from(tabContents)[index].classList.add("tab-content--active");
+    currentTabContent.classList.remove("tab-content--visible");
+    Array.from(tabContents)[index].classList.add("tab-content--visible");
   });
 });
 
@@ -52,24 +52,24 @@ const indicators = document.querySelectorAll(".carousel__indicator");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const offset = button.classList.contains("carousel__button--back") ? -1 : 1;
-    const slides = button.closest(".carousel").querySelector(".carousel__list");
-    const activeSlide = slides.querySelector(".quote--active");
-    const activeIndicator = document.querySelector(
-      ".carousel__indicator--active"
+    const offset = button.classList.contains("carousel__button-back") ? -1 : 1;
+    const quotes = button.closest(".carousel").querySelector(".carousel__list");
+    const selectedQuote = quotes.querySelector(".quote--selected");
+    const selectedIndicator = document.querySelector(
+      ".carousel__indicator--selected"
     );
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    let newIndex = [...quotes.children].indexOf(selectedQuote) + offset;
 
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
+    if (newIndex < 0) newIndex = quotes.children.length - 1;
+    if (newIndex >= quotes.children.length) newIndex = 0;
 
-    slides.children[newIndex].classList.add("quote--active");
-    delete activeSlide.classList.remove("quote--active");
+    quotes.children[newIndex].classList.add("quote--selected");
+    delete selectedQuote.classList.remove("quote--selected");
 
-    activeIndicator?.classList.remove("carousel__indicator--active");
-    indicators[newIndex].classList.add("carousel__indicator--active");
+    selectedIndicator?.classList.remove("carousel__indicator--selected");
+    indicators[newIndex].classList.add("carousel__indicator--selected");
 
-    activeIndicator.textContent = "\u25CB";
+    selectedIndicator.textContent = "\u25CB";
     indicators[newIndex].textContent = "\u25CF";
   });
 });
