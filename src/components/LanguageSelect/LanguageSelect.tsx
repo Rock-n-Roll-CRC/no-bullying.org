90a79c0dfe71962c9b78bb6f23df10c24aef781d
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2025-04-10 11:50:20 +0700

    ✨ Make LanguageSelect component interactive

--- src/components/LanguageSelect/LanguageSelect.tsx
+++ src/components/LanguageSelect/LanguageSelect.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 
 import { merriweather } from "@/utilities/fonts";
@@ -7,19 +10,30 @@ import GlobeSVG from "@/assets/icons/globe.svg";
 import styles from "./LanguageSelect.module.scss";
 
 const LanguageSelect = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggleOpen() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
   return (
     <div className={styles["language-select"]}>
-      <button className={styles["language-select__button"]}>
+      <button
+        onClick={handleToggleOpen}
+        className={styles["language-select__button"]}
+      >
         <GlobeSVG />
       </button>
 
-      <div
-        className={`${styles["language-select__body"] ?? ""} ${merriweather.className}`}
-      >
-        <Link href="/">english</Link>
+      {isOpen && (
+        <div
+          className={`${styles["language-select__body"] ?? ""} ${merriweather.className}`}
+        >
+          <Link href="/">english</Link>
 
-        <Link href="/ru">russian</Link>
-      </div>
+          <Link href="/ru">russian</Link>
+        </div>
+      )}
     </div>
   );
 };
